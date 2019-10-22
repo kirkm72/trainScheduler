1f27c7ecb39f61a15db38c4158dccfcef9f0f652
Kirk McGlamery <kirkm72@att.net>
2019-10-21 19:12:56 -0500

    trouble getting row to remove from DOM

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -8,7 +8,7 @@ var fbaseCfg = { // Firebase info below is unique for every firebase DB setup
     appId: "1:249684455785:web:0f232f5e1b138fade4cdf8"
 };
 firebase.initializeApp(fbaseCfg); // Initialize Firebase
-console.log("Firebase config completed");
+//console.log("Firebase config completed");
 
 var addCity=""; // These variables must be in root scopr or second event listener will not see value
 var addName="";
@@ -38,17 +38,48 @@ $(document).on("click", ".addBtn", function (event) {
     });
 
     // Update DOM table with listed trains
-    
+    let row = $('<tr>');
+    let th = $('<th scope="row">'); //Header will have same classname as addName
+    let td1 = $('<td>'); // Destination City
+    let td2 = $('<td>'); // Time Interval
+    let td3 = $('<td>'); // 1st Time
+    let td4 = $('<td>'); // Military time TBD moment JS
+    let td5 = $('<td>'); // Standard Time TND moment JS
+    let td6 = $('<td>'); // Removal Button
+    th.text(addName);    // Train Name
+    th.addClass(addName); // Header will have same classname as addName
+    td1.text(addCity);    // Destination City
+    td2.text(addInt);     // Time Interval
+    td3.text(addTime);  // First Time
+    td4.text('military');   // Military time TBD moment JS
+    td5.text('standard');   // Standard Time TND moment JS
+    td6.addClass('btn btn-primary remove'); //Removal button classes
+    td6.text(addName);  // Button Text
+    row.append(th);
+    row.append(td1);
+    row.append(td2);
+    row.append(td3);
+    row.append(td4);
+    row.append(td5);
+    row.append(td6);
+    $("#table").append(row); //works
 
     trainDbase.ref('train/').once('value').then(function (snapshot) { // investigate diff between on & once
     console.log(snapshot.val()); //read from firebase
     });
 }); //submit button braces
 
-$(document).on("click", ".remove", function (event) {
-    event.preventDefault();
-    console.log("inside remove button listener looking for ", addName);
-    trainDbase.ref("train/"+addName).remove();
+$(document).on("click", ".remove", function (event) { // Remove from Fbase & DOM
+    event.preventDefault(); // First part removed from Fbase
+    let value = $(this).text();
+    // console.log(value);
+    trainDbase.ref("train/"+value).remove(); //succesfully removed from Fbase
+    // let tmp = "." + this;
+    // console.log(tmp);
+    if($('<th>').hasClass(this)){
+        $('<th>').remove();
+    };
+
 });
 
 }); // doc.ready braces
