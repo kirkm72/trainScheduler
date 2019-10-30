fc2921afad071dfa9131fab4bfafa0baafa6adc5
Kirk McGlamery <kirkm72@att.net>
2019-10-30 16:16:45 -0500

    completed and debugged

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -8,78 +8,85 @@ var fbaseCfg = { // Firebase info below is unique for every firebase DB setup
     appId: "1:249684455785:web:0f232f5e1b138fade4cdf8"
 };
 firebase.initializeApp(fbaseCfg); // Initialize Firebase
-//console.log("Firebase config completed");
 
-var addCity=""; // These variables must be in root scopr or second event listener will not see value
-var addName="";
-var addInt=0;
-var addTime="";
-var removeName="";
+var addCity = ""; // These variables must be in root scope or second event listener will not see value
+var addName = "";
+var addInt = 0;
+var addTime = "";
+var removeName = "";
+
+setInterval(() => { // Interval set for timer to seed MomentJS
+    const now = moment();
+    let mTime = now.format('HH:mm:ss');  //Military time format
+    let sTime = now.format('h:mm:ss A'); //Standard time format
+    $('#mClock').text(mTime);
+    $('#sClock').text(sTime);
+}, 1000);
 
 $(document).ready(function () {
     trainDbase = firebase.database();
 
-// Form input section
-$(document).on("click", ".addBtn", function (event) { 
-    event.preventDefault();
-    addCity = $("#inputCity").val().trim();
-    addName = $("#inputName").val().trim();
-    addInt = $("#inputInterval").val();
-    addTime = $("#inputTime").val().trim();
-    console.log("before sent to fBase", addName, addCity, addInt, addTime);
-    $("#form").get(0).reset(); //native javascript: resets input form after submission
+    // Form input section
+    $(document).on("click", ".addBtn", function (event) { // Even listener for submit button
+        event.preventDefault();
+        addCity = $("#inputCity").val().trim();
+        addName = $("#inputName").val().trim();
+        addInt = $("#inputInterval").val();
+        addTime = $("#firstTrain").val();
+        
+        trainDbase.ref('train/' + addName).update({ //set overwrites to firebase, use update
+            name: addName,
+            city: addCity,
+            interval: addInt,
+            time: addTime
+        });
+        $("#form").get(0).reset(); //native javascript: resets input form after submission. 
+        //Must be placed after firebase write or else will input the "option selected" field in to fBase.
+        //See line 70 of index.html
 
-    // Writes to fBase
-    trainDbase.ref('train/'+addName).update({ //set overwrites to firebase, use update
-        name: addName,
-        city: addCity,
-        interval: addInt,
-        time: addTime
+    }); //submit button braces
+
+    trainDbase.ref('train/').on("child_added", function (snapshot, prevChildKey) {
+        // Update DOM table with listed trains
+        let Name = snapshot.val().name;
+        let City = snapshot.val().city;
+        let Int = snapshot.val().interval;
+        let firstTime = snapshot.val().time;
+        var now = moment();
+
+        // Update DOM
+        let row = $('<tr>');
+        let th = $('<th scope="row">'); //Header will have same classname as addName
+        let td1 = $('<td>');  // Origin City
+        let td2 = $('<td>');  // Time Interval
+        let td3 = $('<td>');  // 1st Time
+        let td4 = $('<td>');  // Military time TBD moment JS
+        th.text(Name);     // Train Name
+        th.addClass(Name); // Header will have same classname as addName
+        td1.text(City);    // Origin City
+        td2.text(Int + "th Minute");     // Time Interval
+        if (firstTime === "00:00") { // Conditionally show "Noon" or "Midnight"
+            td3.text("Midnight");
+        } else if (firstTime === "12:00") {
+            td3.text("Noon");
+        } else {
+            td3.text(firstTime);
+        };
+        if (Int >= (moment().minute())) {
+            let next = Int - moment().minute();
+            td4.text(next + " Minutes");
+        }
+        if (Int < now.minute()){
+            let next = 60 - parseInt(now.minute()) + parseInt(Int); 
+            td4.text(next + " Minutes");
+        }
+        row.append(th);
+        row.append(td1);
+        row.append(td2);
+        row.append(td3);
+        row.append(td4);
+        row.addClass(addName);
+        $("#table").append(row); //works
+
     });
-
-    // Update DOM table with listed trains
-    let row = $('<tr>');
-    let th = $('<th scope="row">'); //Header will have same classname as addName
-    let td1 = $('<td>'); // Destination City
-    let td2 = $('<td>'); // Time Interval
-    let td3 = $('<td>'); // 1st Time
-    let td4 = $('<td>'); // Military time TBD moment JS
-    let td5 = $('<td>'); // Standard Time TND moment JS
-    let td6 = $('<td>'); // Removal Button
-    th.text(addName);    // Train Name
-    th.addClass(addName); // Header will have same classname as addName
-    td1.text(addCity);    // Destination City
-    td2.text(addInt);     // Time Interval
-    td3.text(addTime);  // First Time
-    td4.text('military');   // Military time TBD moment JS
-    td5.text('standard');   // Standard Time TND moment JS
-    td6.addClass('btn btn-primary remove'); //Removal button classes
-    td6.text(addName);  // Button Text
-    row.append(th);
-    row.append(td1);
-    row.append(td2);
-    row.append(td3);
-    row.append(td4);
-    row.append(td5);
-    row.append(td6);
-    $("#table").append(row); //works
-
-    trainDbase.ref('train/').once('value').then(function (snapshot) { // investigate diff between on & once
-    console.log(snapshot.val()); //read from firebase
-    });
-}); //submit button braces
-
-$(document).on("click", ".remove", function (event) { // Remove from Fbase & DOM
-    event.preventDefault(); // First part removed from Fbase
-    let value = $(this).text();
-    // console.log(value);
-    trainDbase.ref("train/"+value).remove(); //succesfully removed from Fbase
-    // let tmp = "." + this;
-    // console.log(tmp);
-    if($('<th>').hasClass(this)){
-        $('<th>').remove();
-    };
-
-});
-
 }); // doc.ready braces
